81e4e37bd2da9d1b7423f3a3beff7863b6956760
mdelacre <mdelacre@protonmail.com>
2022-04-04 19:48:06 +0200

    fix close modal

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,13 +11,19 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const crossBtn = document.querySelectorAll(".close");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+crossBtn.forEach((btn) => btn.addEventListener("click", closeModal));
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
 
+//cacher le modal
+function closeModal(){
+  modalbg.style.display = "none";
+}
 
